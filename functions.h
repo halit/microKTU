c4a2d91df8ad6c3276d0e6536e66c1031358089e
Halit <info@halitalptekin.com>
2012-12-12 23:37:49 +0200

    functions added

---
+++ functions.h
@@ -0,0 +1,129 @@
+#include "variables.h"
+
+/* Kuzeye Hareket */
+void moveKuzey(){
+	turnKuzey();
+	moveIleri();
+}
+
+/* Guneye Hareket */
+void moveGuney(){
+	turnGuney();
+	moveIleri();
+}
+
+/* Batiya Hareket */
+void moveBati(){
+	turnBati();
+	moveIleri();
+}
+
+/* Doguya Hareket */
+void moveDogu(){
+	turnDogu();
+	moveIleri();
+}
+
+/* Kuzeye Donus */
+void turnKuzey(){
+	switch(robotunYonu){
+		case 0:
+			solaDon(); // @TODO: stabilize olmali
+			break;
+
+		case 1:
+			geriyeDon(); // @TODO: stabilize olmali
+			break;
+
+		case 2:
+			break;
+
+		case 3:
+			sagaDon(); // @TODO: stabilize olmali
+			break;
+
+	}
+
+}
+
+/* Guneye Donus */
+void turnGuney(){
+	switch(robotunYonu){
+		case 0:
+			sagaDon(); // @TODO: stabilize olmali
+			break;
+
+		case 1:
+			break;
+
+		case 2:
+			geriyeDon(); // @TODO: stabilize olmali
+			break;
+
+		case 3:
+			solaDon(); // @TODO: stabilize olmali
+			break;
+
+	}	
+
+}
+
+/* Batiya Donus */
+void turnBati(){
+	switch(robotunYonu){
+		case 0:
+			geriyeDon(); // @TODO: stabilize olmali
+			break;
+
+		case 1:
+			sagaDon(); // @TODO: stabilize olmali
+			break;
+
+		case 2:
+			solaDon(); // @TODO: stabilize olmali
+			break;
+
+		case 3:
+			break;
+
+	}	
+
+}
+
+/* Doguya Donus */
+void turnDogu(){
+	switch(robotunYonu){
+		case 0:
+			break;
+
+		case 1:
+			solaDon(); // @TODO: stabilize olmali
+			break;
+
+		case 2:
+			sagaDon(); // @TODO: stabilize olmali
+			break;
+
+		case 3:
+			geriyeDon(); // @TODO: stabilize olmali
+			break;
+
+	}		
+
+}
+
+void sagaDon(){
+
+}
+
+void solaDon(){
+
+}
+
+void geriyeDon(){
+
+}
+
+void moveIleri(){
+	
+}
---
+++ variables.h
@@ -0,0 +1,5 @@
+/* (0->Dogu) (3->Bati) (2->Kuzey) (1->Guney) */
+int robotunYonu = 0; // robotun o anki yonu
+int simdikiKonumDegeri = 14; // bulundugu konumun degeri
+int harita[7][7]; // harita
+
